32a0cc3d8b0a71cb362bf2ec8daf4fc2552a3444
Jens <63097153+eonjs@users.noreply.github.com>
2020-07-07 14:22:40 +0200

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "updating runner"
-sudo apt update && sudo apt upgrade -y
+# comment cause reasons...
+# echo "updating runner"
+# sudo apt update && sudo apt upgrade -y
 
 echo "installing python3"
 sudo apt install python3 -y
+
+echo "install python3-pip"
+sudo apt install python3-pip -y
